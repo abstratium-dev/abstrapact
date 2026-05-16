a7591fc12f323163f4dd200965c0cb8f0f201aed
Ant Kutschera <1108507+maxant@users.noreply.github.com>
2026-05-17 00:07:30 +0200

    removing dependency on roles coz package will be changed when user implements their own service; fix test

--- src/main/java/dev/abstratium/core/BuildInfo.java
+++ src/main/java/dev/abstratium/core/BuildInfo.java
@@ -18,7 +18,7 @@ public final class BuildInfo {
      * This value is automatically updated by .git/hooks/pre-commit
      * DO NOT EDIT MANUALLY - changes will be overwritten
      */
-    public static final String BUILD_TIMESTAMP = "2026-05-16T20:44:43Z";
+    public static final String BUILD_TIMESTAMP = "2026-05-16T22:07:30Z";
     
     private BuildInfo() {
         // Utility class - prevent instantiation
